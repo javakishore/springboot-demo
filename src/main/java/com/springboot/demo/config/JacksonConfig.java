e73e8a754978c44ba7c91ea5b356c7e9211446fc
Kishore <kishore.jannarapu@gmail.com>
2018-12-17 23:36:30 +0800

    Added Serialise the LocalDateTime / Timestamp

--- src/main/java/com/springboot/demo/config/JacksonConfig.java
+++ src/main/java/com/springboot/demo/config/JacksonConfig.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.springboot.demo.mixin.UserMixin;
 import com.springboot.demo.model.User;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,14 @@ public class JacksonConfig {
   public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
     Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
     jackson2ObjectMapperBuilder.mixIn(User.class, UserMixin.class);
+
     return jackson2ObjectMapperBuilder;
   }
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        System.out.println("Config is starting.");
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return objectMapper;
+    }
 }
