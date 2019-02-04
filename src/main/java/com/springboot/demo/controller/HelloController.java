3f347ba6034b836989ac794d6147d0fc37fad256
Kishore <kishore.jannarapu@gmail.com>
2019-02-04 15:16:59 +0800

    Log4j implementation

--- src/main/java/com/springboot/demo/controller/HelloController.java
+++ src/main/java/com/springboot/demo/controller/HelloController.java
@@ -4,12 +4,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest")
 @Api(value = "HelloWorld Resource", description = "shows hello world")
 public class HelloController {
+  Logger logger = LoggerFactory.getLogger(getClass());
 
   @ApiOperation(value = "Returns Hello World")
   @ApiResponses(
@@ -19,10 +22,19 @@ public class HelloController {
       })
   @GetMapping("/hello")
   public String hello() {
-    return "Hello World ";
+      logger.info("HelloController >> hello called");
+    System.out.println("HelloController >> hello called");
+      return "Hello World ";
   }
+    @GetMapping("/error")
+    public String error() throws Exception{
+        logger.info("HelloController >> hello called");
+        System.out.println("HelloController >> hello called");
+        throw new Exception("Exception occured");
+    }
 
-  @ApiOperation(value = "Returns Hello World")
+
+    @ApiOperation(value = "Returns Hello World")
   @PostMapping("/post")
   public String helloPost(@RequestBody final String hello) {
     return hello;
